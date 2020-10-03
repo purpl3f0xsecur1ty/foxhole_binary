f653291a72587580a535543cd58d95d6750018bf
purpl3-f0x <42351007+purpl3-f0x@users.noreply.github.com>
2020-10-03 15:19:50 -0400

    Update GiveMeRootPlz.c

--- GiveMeRootPlz.c
+++ GiveMeRootPlz.c
@@ -3,18 +3,23 @@
 #include <unistd.h>
 
 int secret() {
-	uid_t uid = geteuid();
+	//Make sure bash isn't dropping permissions
+	uid_t uid = geteuid(); 
 	setresuid(uid, uid, uid);
 	gid_t gid = getegid();
 	setresgid(gid, gid, gid);
+	//Execute bash
 	return system("/bin/bash");
 }
 
 int overflow() {
 	printf("\nDo you want the root password?");
-	char buffer[500];
+	//Making a buffer of 500 bytes
+	char buffer[500]; 
 	int userinput;
-	userinput = gets(buffer);
+	//Use a vulnerable function to overflow the "buffer" variable
+	//Gets() doesn't do bounds checking, and doesn't terminate on a null byte (\x00)
+	userinput = gets(buffer); 
 	printf("\nYou didn't convince me!");
 	printf("\nMaybe you should write me a *very long* reason why I should give you the password\n");
 	return 0;
